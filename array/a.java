0e8ce7de5efa22fe4d0e04f67b5c93943f9980c9
lakshyajitusir <jitu.sahoo54@gmail.com>
2026-06-06 08:39:28 +0530

    my thirteenth day

---
+++ array/a.java
@@ -0,0 +1,20 @@
+ import java.util.*;
+ClassA
+{
+	public static void main (string arg[])
+	{
+		Scanner SC =new scanner (System.in) 
+		System.out.Println("enter two Number");
+		int a=Sc.Nextin +(),b=sc.Nextint();
+		System.out.Println("first no="+a);
+		System.out.Println("second no="+b);
+		//System.out.Println("add="+a+b)add 1020
+		System.out.Println("add="+(a+b));
+		System.out.Println("mult="+a*b);
+
+
+
+
+
+
+	}
